93124d0cd9f7b82bdb6c20a38e7d7e4b25a67f28
thalesmenegueco <thales.menegueco@gmail.com>
2026-08-02 21:00:23 -0300

    fix: update of plugins for Tailwind v4

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -30,7 +30,7 @@ export default async function DashboardPage() {
         <div className="max-w-6xl mx-auto p-8 pb-8 font-inter">
             <header className="mb-16 flex flex-col justify-between gap-2">
                 <div>
-                    <h1 className="text-5xl font-bold text-white mb-3 font-atkinson">Os Meus Discursos</h1>
+                    <h1 className="text-5xl font-bold text-white mb-3 font-atkinson">Meus Discursos</h1>
                     <p className="text-lg text-[#b7c1de]/90">Selecione um esboço para iniciar a apresentação.</p>
                 </div>
                 <div className="flex gap-3">
